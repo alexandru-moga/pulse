7f56cb557fe540e9b34f9381d0d73798bf0a790e
Alexandru Moga <98146541+alexandru-moga@users.noreply.github.com>
2025-05-10 04:48:30 +0300

    Chaged the code to react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "Los Angeles", "Chicago"];
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
