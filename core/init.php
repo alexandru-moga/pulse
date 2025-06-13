a426773e4724a02f4d2dcc4d569665f907de8e2c
Alexandru Moga <98146541+alexandru-moga@users.noreply.github.com>
2025-06-13 22:13:10 +0300

    Update init.php

--- core/init.php
+++ core/init.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', getenv('DB_HOST') ?: 'mysql.railway.internal:3306');
-define('DB_NAME', getenv('DB_NAME') ?: 'railway');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: 'password');
+define('DB_HOST', 'server:port');
+define('DB_NAME', 'database');
+define('DB_USER', 'username');
+define('DB_PASS', 'password');
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
