58a1fcbe308f9701ca905df1536dae6b7d36a591
Alexandru Moga <98146541+alexandru-moga@users.noreply.github.com>
2025-06-13 21:34:49 +0300

    Removed config file.

--- core/init.php
+++ core/init.php
@@ -1,5 +1,8 @@
 <?php
-require_once __DIR__ . '/../config.php';
+define('DB_HOST', getenv('DB_HOST') ?: 'mysql.railway.internal:3306');
+define('DB_NAME', getenv('DB_NAME') ?: 'railway');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: 'password');
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
