fe3357132bc91eb6f1b290c1034252bdd845a8d9
Alexandru Moga <98146541+alexandru-moga@users.noreply.github.com>
2025-09-10 21:39:21 +0300

    .

--- components/sections/stickers.php
+++ components/sections/stickers.php
@@ -1,11 +1,13 @@
 <?php
-// JukeBox Stickers Component - Minimal Required Link
+// JukeBox Stickers Component - Card Style
 ?>
 
-<section class="py-8 bg-gray-50 dark:bg-gray-900 text-center">
+<section class="py-8 bg-transparent">
     <div class="max-w-4xl mx-auto px-4">
-        <p class="text-lg text-gray-700 dark:text-gray-300">
-            Thanks to JukeBox for our <a href="https://www.jukeboxprint.com/custom-stickers" class="text-red-500 hover:text-red-600 font-medium underline transition-colors">custom stickers</a>!
-        </p>
+        <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-8 text-center border border-gray-200 dark:border-gray-700 hover:shadow-xl transition-shadow duration-300">
+            <p class="text-lg text-gray-700 dark:text-gray-300">
+                Thanks to JukeBox for our <a href="https://www.jukeboxprint.com/custom-stickers" class="text-red-500 hover:text-red-600 font-medium underline transition-colors">custom stickers</a>!
+            </p>
+        </div>
     </div>
 </section>
